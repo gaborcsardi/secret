27213b6c258f195153a556b8fc29ac446350f13b
Andrie de Vries <apdevries@gmail.com>
2017-03-05 22:17:16 +0000

    Document local_key() and create_vault()

--- R/package.R
+++ R/package.R
@@ -2,7 +2,7 @@
 #' Share Sensitive Information in R Packages
 #'
 #' Allow sharing sensitive information, for example passwords, API keys,
-#' etc., in R packages, using public key cryptography.
+#' or other information in R packages, using public key cryptography.
 #'
 #' A vault is a directory, typically inside an R package, that
 #' stores a number of secrets. Each secret is shared among a group of
@@ -11,11 +11,14 @@
 #' The package implements the following operations:
 #' 
 #' * Vault:
+#'   - Creating a vault folder: [create_vault()]
 #'   - Creating a package vault: [create_package_vault()]
 #' * User management:
 #'   - Adding a user: [add_user()], [add_github_user()].
 #'   - Deleting a user: [delete_user()].
-#'   - List users: [list_users()].
+#'   - Listing users: [list_users()].
+#' * Keys:
+#'   - Reading local private key: [local_key()]
 #' * Secrets:
 #'   - Adding a secret: [add_secret()].
 #'   - Retrieving a secret: [get_secret()].
@@ -24,7 +27,7 @@
 #'   - List secrets: [list_secrets()].
 #'   - Sharing a secret: [share_secret()]. Query or set the set of
 #'   users that have access to a secret.
-#'   - Unshare a secret: [unshare_secret()]
+#'   - Unsharing a secret: [unshare_secret()]
 #'
 #' @docType package
 #' @keywords package
