6eff77690665f63d3769fb29f75e76112dc4ce15
Gábor Csárdi <csardi.gabor@gmail.com>
2017-06-13 14:07:24 +0100

    Test improvements:
    * mock local_key test for consistent results
    * use withr to restore USER_KEY env var properly

--- tests/testthat/test-5-keys.R
+++ tests/testthat/test-5-keys.R
@@ -2,27 +2,32 @@
 context("local key")
 
 test_that("can read local key", {
-  skip_on_cran()
-  z <- tryCatch(local_key(), error = function(e)e)
+
+  mockery::stub(local_key, "Sys.getenv", "sdfqrtafgaetsgsfgqr")
+  z <- tryCatch(local_key(), error = function(e) e)
   if (inherits(z, "error")) skip("No local key available")
+
+  pth <- system.file("user_keys/alice.pem", package = "secret")
+  z <- mockery::stub(local_key, "Sys.getenv", pth)
   
-  expect_is(local_key(), "key")
+  expect_is(z, "key")
 })
 
 test_that("can read local key when setting env variable", {
-  old_env <- Sys.getenv("USER_KEY")
-  on.exit(Sys.setenv(USER_KEY = old_env))
-  
+
   pth <- system.file("user_keys/alice.pem", package = "secret")
-  Sys.setenv(USER_KEY = pth)
-  z <- local_key()
+  z <- withr::with_envvar(
+    c(USER_KEY = pth),
+    local_key()
+  )
   expect_is(z, "key")
   expect_is(z, "rsa")
-  
-  Sys.setenv(USER_KEY = "path/does/not/exist")
-  expect_error(
-    local_key(),
-    "No suitable user key found."
+
+  withr::with_envvar(
+    c(USER_KEY = "path/does/not/exist"),
+    expect_error(
+      local_key(),
+      "No suitable user key found."
+    )
   )
-  
 })
